83c4d4ab3c9a0e1742e1891f86d3a9701a2f2897
Jenni Mäkinen <jenni.makinen70@gmail.com>
2021-11-19 08:16:18 +0200

    4.2.

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
-app.use(cors())
-app.use(express.json())
-const mongoose = require('mongoose')
+//const http = require('http')
+//const express = require('express')
+//require('dotenv').config()
+//const app = express()
+//const cors = require('cors')
+//app.use(cors())
+//app.use(express.json())
+//const mongoose = require('mongoose')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const mongoUrl = process.env.MONGODB_URI
+/*const mongoUrl = process.env.MONGODB_URI
 console.log('connecting to', mongoUrl)
 mongoose.connect(mongoUrl)
   .then(result => {
@@ -17,35 +20,11 @@ mongoose.connect(mongoUrl)
     console.log('error connecting to MongoDB:', error.message)
   })
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
 const Blog = mongoose.model('Blog', blogSchema)
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+*/
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
