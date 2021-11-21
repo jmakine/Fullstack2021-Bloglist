8245b4f2a9a09db484e7c71a4fcfcfd4c675822e
Jenni Mäkinen <jenni.makinen70@gmail.com>
2021-11-21 13:48:55 +0200

    4.5.

---
+++ tests/favoriteBlog.test.js
@@ -0,0 +1,31 @@
+const favoriteBlog = require('../utils/favoriteBlog')
+
+describe('favorite', () => {
+    test('favorite blog', () => {
+        
+        const blogs = [
+            {
+            title: "Test 2",
+            author: "2",
+            url: "2",
+            likes: 2,
+            },
+            {
+            title: "Test 3",
+            author: "3",
+            url: "3",
+            likes: 3,
+            }
+        ]
+        
+        const maxLikesBlog = [{
+            title: "Test 3",
+            author: "3",
+            url: "3",
+            likes: 3,
+        }]
+
+        const result = favoriteBlog.favoriteBlog(blogs)
+        expect(result).toEqual(maxLikesBlog)    
+    })
+})
