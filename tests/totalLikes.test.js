f2f568f5e84d0f4542ff86c728a0e3dd5730b6de
Jenni Mäkinen <jenni.makinen70@gmail.com>
2021-11-21 12:59:02 +0200

    4.4.

---
+++ tests/totalLikes.test.js
@@ -0,0 +1,31 @@
+const totalLikes = require('../utils/totalLikes')
+
+const blogs_2_and_3 = [
+    {
+    title: "Test 2",
+    author: "2",
+    url: "2",
+    likes: 2,
+    },
+    {
+    title: "Test 3",
+    author: "3",
+    url: "3",
+    likes: 3,
+    }
+]
+
+const empty_list = []
+
+describe('total likes', () => {
+    test('2 + 3 = 5', () => {
+    const result = totalLikes.sumOfLikes(blogs_2_and_3)
+    expect(result).toBe(5)
+    })
+
+    test('empty list retuns 0', () => {
+        const result = totalLikes.sumOfLikes(empty_list)
+        expect(result).toBe(0)
+    })
+
+})
---
+++ utils/totalLikes.js
@@ -0,0 +1,12 @@
+const sumOfLikes = (blogs) => {
+    if (blogs.length != 0) {
+     return blogs
+            .map(item => item.likes)
+            .reduce((prev, next) => prev + next)
+    }
+    else return 0
+}
+
+module.exports = {
+    sumOfLikes
+}
