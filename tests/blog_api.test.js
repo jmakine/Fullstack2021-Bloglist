8a11df253c58ed6ba5bc3b2edca6ccf29e12cacd
Jenni Mäkinen <jenni.makinen70@gmail.com>
2021-12-04 06:40:55 +0200

    4.15.

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -4,6 +4,7 @@ const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 describe('get blogs', () => {
 
@@ -47,6 +48,60 @@ describe('adding a blog', () => {
 
     const titles = blogsAfter.map(n => n.title)
     expect(titles).toContain('Add new')
+  })
+
+})
+
+describe('when there is initially one user at db', () => {
+  
+  beforeEach(async () => {
+    await User.deleteMany({})
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'jenska',
+      name: 'Jenni Mäkinen',
+      password: 'password123',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAfter = await helper.usersInDb()
+    expect(usersAfter).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAfter.map(user => user.username)
+    expect(usernames).toContain(newUser.username)
+  })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('`username` to be unique')
+
+    const usersAfter = await helper.usersInDb()
+    expect(usersAfter).toHaveLength(usersAtStart.length)
   })
 
 })
